b7a0f3595add8ad9fe06168189087e38d41588d2
SaraUlan <220872@astanait.edu.kz>
2023-04-08 23:48:30 +0600

    permutations  7task

---
+++ src/main/java/Permute.java
@@ -0,0 +1,28 @@
+IOimport java.util.Scanner;
+
+class Permute {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
+
+        permute(str.toCharArray(), 0);
+    }
+
+    public static void permute(char[] arr, int ind) {
+        if (ind == arr.length - 1) {
+            System.out.println(new String(arr));
+            return;
+        }
+        for (int i = ind; i < arr.length; i++) {
+            swap(arr, ind, i);
+            permute(arr, ind + 1);
+            swap(arr, ind, i);
+        }
+    }
+
+    public static void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
