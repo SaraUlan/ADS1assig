d5a51957a1b6b0d37ee8b49676f9f2bb74a11111
SaraUlan <220872@astanait.edu.kz>
2023-04-08 12:04:41 +0600

    find min

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        int[] arr = new int[num];
+        for (int i = 0; i < num; i++) {
+            arr[i] = input.nextInt();
+        }
+        int min = findM(arr,num);
+        System.out.println(min);
+    }
+    public static int findM(int[] arr,int num){
+        if(num < 2){
+            return arr[0];
+        }
+        int min = findM(arr,num-1);
+        if(arr[num-1] < min){
+            min = arr[num-1];
+        }
+        return min;
+    }
+}
